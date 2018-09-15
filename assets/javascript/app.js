be2919e251691c4b0c7750b0bbbd3974f2019541
BikeMixel <bixelmike@gmail.com>
2018-09-15 08:51:18 -0400

    need logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,6 @@ $("#frmSubmit").on("click", function(event){
     event.preventDefault()
 
     var name = $("#trainName").val().trim()
-    console.log($("#trainName").val().trim())
     var dest = $("#trainDest").val().trim()
     var freq = $("#trainFreq").val().trim()
     var next = $("#trainNext").val().trim()
@@ -22,23 +21,27 @@ $("#frmSubmit").on("click", function(event){
         userFreq: freq,
         userNext: next
     })
-})  
-database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
-    popData(snapshot.val())
+})
+
+database.ref().orderByChild("dateAdded").on("child_added", function(snapshot){
+    populate(snapshot.val())
 })
 
 // append data to table from firebase, create new <td>s 
-var addTD = function(data, target) {
-    var newTD = $('<td>');
-    $(newTD).text(data);
+var addTD = function(input, target) {
+    var newTD = $("<td>");
+    $(newTD).text(input);
     $(newTD).appendTo(target)
    }
    
-   var popData = function(dataSet) {
-    var newRow = $('<tr>');
-    addTD(dataSet.userName, newRow)
-    addTD(dataSet.userDest, newRow)
-    addTD(dataSet.userFreq, newRow)
-    addTD("Minutes Left: " + dataSet.userNext, newRow)
+var populate = function(output) {
+    var newRow = $("<tr>");
+    addTD(output.userName, newRow)
+    addTD(output.userDest, newRow)
+    addTD(output.userFreq, newRow)
+    //  need an fn for this one, dont paste this data 
+    //  addTD("Minutes Left: " + output.userNext, newRow)
     $(newRow).appendTo($("#tableBody"))
 }
+
+// use moment for time calc.. 2-3 fns max
