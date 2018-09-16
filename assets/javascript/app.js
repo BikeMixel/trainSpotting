668f54337abe8c42c969d884749fbd86f80bf4d3
BikeMixel <bixelmike@gmail.com>
2018-09-16 00:18:07 -0400

    closer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,67 @@
 // firebase 
 var database = firebase.database()
-
 // submit fun
-$("#frmSubmit").on("click", function(event){
+
+$("#frmSubmit").on("click", function (event) {
     event.preventDefault()
 
     var name = $("#trainName").val().trim()
     var dest = $("#trainDest").val().trim()
     var freq = $("#trainFreq").val().trim()
-    var next = $("#trainNext").val().trim()
+    var start= $("#trainStart").val().trim()
 
     $("#trainName").val("")
     $("#trainDest").val("")
     $("#trainFreq").val("")
-    $("#trainNext").val("")
+    $("#trainStart").val("")
+
+var time = moment().format("HH:mm")
+console.log(time)
+var startTime = moment(start, "HH:mm").format("HH:mm")
+console.log("startTime " + startTime)
+var difference = moment().diff(moment(startTime, "HH:mm"), "minutes")
+console.log("difference " + difference)
+var minLeft = difference % freq
+console.log("minLeft " + minLeft)
 
     database.ref().push({
         userName: name,
         userDest: dest,
         userFreq: freq,
-        userNext: next
+        userNext: start,
+        minLeft: minLeft
     })
 })
 
-database.ref().orderByChild("dateAdded").on("child_added", function(snapshot){
+database.ref().orderByChild("dateAdded").on("child_added", function (snapshot) {
     populate(snapshot.val())
 })
 
 // append data to table from firebase, create new <td>s 
-var addTD = function(input, target) {
-    var newTD = $("<td>");
-    $(newTD).text(input);
+var addData = function (input, target) {
+    var newTD = $("<td>")
+    $(newTD).text(input)
     $(newTD).appendTo(target)
-   }
-   
-var populate = function(output) {
+}
+
+var populate = function (output) {
     var newRow = $("<tr>");
-    addTD(output.userName, newRow)
-    addTD(output.userDest, newRow)
-    addTD(output.userFreq, newRow)
-    //  need an fn for this one, dont paste this data 
-    //  addTD("Minutes Left: " + output.userNext, newRow)
+    addData(output.userName, newRow)
+    addData(output.userDest, newRow)
+    addData(output.userFreq, newRow)
+    addData("Sasdfas", newRow)
+    addData(output.minLeft, newRow)
     $(newRow).appendTo($("#tableBody"))
 }
 
 // use moment for time calc.. 2-3 fns max
+
+
+// var startTest = 2200
+// var time = moment().format("HH:mm")
+// console.log(time)
+// var startTime = moment(startTest, "HH:mm").format("HH:mm")
+// console.log("startTime " + startTime)
+// var difference = moment().diff((startTime), "minutes") % userFreq.freq
+// console.log("difference " + difference)
+
